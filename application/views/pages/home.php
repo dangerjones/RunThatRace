5e89e4d514c843608e7e1292affe00c0b3efc588
dangerjones <weston.cerny@navitaire.com>
2011-12-28 11:13:21 -0700

    added clearBoth divs to make divs cover end

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,25 +1,16 @@
 <div id="wrapper">
 	<img id="tagline" src="/rtrigniter/images/rtr_tag_line_slant_772_66.png" alt="Managing, Finding, Registering &amp; Running That Race was never so easy!">
 	<div id="body">
-		<a href="/rtrigniter/search" class="linkButton">
-			<button id="findRace" class="bigRedButton">find that race</button>
-		</a>
-	  <a href="rtrigniter/signup" class="linkButton">
-			<button id="signupRace" class="bigRedButton">signup for that race</button>
-		</a>
-		<a href="/rtrigniter/create" class="linkButton">
-	    <button id="createRace" class="bigRedButton">create that race</button>
-		</a>
+		<button id="findRace" class="bigRedButton">find that race</button>
+		<button id="signupRace" class="bigRedButton">signup for that race</button>
+    <button id="createRace" class="bigRedButton">create that race</button>
+		<div class="clearBoth"></div>
 	</div>
 	<div id="bodyFooter">
-	  <a href="/rtrigniter/promo/1" class="linkButton">
-		  <button class="bigBlackButton">promo 1</button>
-	  </a>
-	   <a href="rtrigniter/promo/2" class="linkButton">
-		  <button class="bigBlackButton">promo 2</button>
-	  </a>
-	  <a href="/rtrigniter/promo/3" class="linkButton">
-	 	  <button class="bigBlackButton">promo 3</button>
-	  </a>
+	  <button class="bigBlackButton">promo 1</button>
+	  <button class="bigBlackButton">promo 2</button>
+	  <button class="bigBlackButton">promo 3</button>
+		<div class="clearBoth"></div>
 	</div>
+	<div class="clearBoth"></div>
 </div>
