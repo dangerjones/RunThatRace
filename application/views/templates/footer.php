5afe7f6a25b2b97a6a3ec6cda403f8608b57883b
dangerjones <weston.cerny@navitaire.com>
2011-12-28 10:50:26 -0700

    change buttons to 200 pixels

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,7 @@
-			<div id="ft" role="contentinfo">
-				<p class="centerText">&copy; Copyright 2012 Run That Race</p>
-				<p class="centerText">Date Created: <?php echo date("D, d M Y H:i:s") ?></p>
-			</div>
+		<div class="clearBoth"></div>
+		<div id="ft">
+			<p class="centerText">&copy; Copyright 2012 Run That Race</p>
+			<p class="centerText">Date Created: <?php echo date("D, d M Y H:i:s") ?></p>
 		</div>
 	</body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,15 +1,13 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
  "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 	<head>
   	<title>Run That Race</title>
-  	<link rel="stylesheet" href="http://yui.yahooapis.com/2.8.0r4/build/reset-fonts-grids/reset-fonts-grids.css" type="text/css" />
-		<link rel="stylesheet" href="/rtrigniter/css/app.css" type="text/css" />
+		<link rel="stylesheet" href="/rtrigniter/css/app.css" type="text/css">
 	</head>
-	<body class="yui-skin-sam">
-		<div id="doc" class="yui-t7">
-   		<div id="hd" role="banner">
-				<a href="/">
-					<img src="/rtrigniter/images/rtr_header_logo_205_72.png" alt="home"/>
-				</a>
-			</div>
+	<body>
+  	<div id="hd">
+			<a href="/">
+				<img src="/rtrigniter/images/rtr_header_logo_205_72.png" alt="home">
+			</a>
+		</div>
